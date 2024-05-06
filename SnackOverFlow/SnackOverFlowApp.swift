71dd9e19a6b538aa17d8c00498f1061766fa9907
furkanzaif <zaiffurkan@gmail.com>
2024-05-06 13:46:07 +0300

    Initial Commit

---
+++ SnackOverFlow/SnackOverFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnackOverFlowApp.swift
+//  SnackOverFlow
+//
+//  Created by Furkan Zaif on 6.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct SnackOverFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
